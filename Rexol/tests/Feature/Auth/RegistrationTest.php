321ee7b447d2e64c362a80e243f03f1aff97c8cd
rifathossainshan <rifathossainshan@gmail.com>
2025-12-09 00:16:00 +0600

    feat: implement user registration and authentication controllers with corresponding tests.

--- Rexol/tests/Feature/Auth/RegistrationTest.php
+++ Rexol/tests/Feature/Auth/RegistrationTest.php
@@ -21,11 +21,12 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'phone' => '1234567890',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertRedirect(route('home', absolute: false));
     }
 }
