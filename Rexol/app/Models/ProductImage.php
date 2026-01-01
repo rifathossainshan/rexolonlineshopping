8eb664ebf114b0f3d47bc3d4d184d85ea4aa0445
rifathossainshan <rifathossainshan@gmail.com>
2026-01-02 00:55:21 +0600

    h

--- Rexol/app/Models/ProductImage.php
+++ Rexol/app/Models/ProductImage.php
@@ -12,17 +12,4 @@ class ProductImage extends Model
     {
         return $this->belongsTo(Product::class);
     }
-
-    public function getImageAttribute($value)
-    {
-        if (str_starts_with($value, 'http')) {
-            return $value;
-        }
-
-        if (str_starts_with($value, 'storage/')) {
-            return asset($value);
-        }
-
-        return asset('storage/' . $value);
-    }
 }
